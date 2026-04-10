2b01ee26d80f3521a59b20002be92fbd8c78bad7
firetadpole <jesseeasley952@gmail.com>
2026-04-10 15:32:55 -0700

    set up necessary libraries

--- asgn1/src/asgn1.js
+++ asgn1/src/asgn1.js
@@ -1,3 +1,18 @@
-function main(){
-    console.log("Error: Main not implemented!!")
+// HelloCanvas.js (c) 2012 matsuda
+function main() {
+  // Retrieve <canvas> element
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
+
+  // Set clear color
+  gl.clearColor(0.5, 0.7, 0.8, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
